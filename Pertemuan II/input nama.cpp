f5ecfb64ab8d8586bee6dd7b2ed7a3320d47fb62
Dedy Mulyana <32387341+Dedymulyana@users.noreply.github.com>
2017-10-09 23:57:06 +0700

    Add files via upload

---
+++ Pertemuan II/input nama.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	string nama;
+	cout << "Masukan Nama : ";
+	cin >> nama;
+	cout << " Nama Adalah : " + nama;
+
+	return 0;
+}
---
+++ Pertemuan II/langkah memasak Nasi.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+cout<< "==============================" <<endl;
+cout<< "cara memasak nasi" <<endl;
+cout<< "==============================" <<endl;
+
+cout<< "1. ambil wadah" <<endl;
+cout<< "2. masukan beras ke dalam wadah" <<endl;
+cout<< "3. masukan air ke dalam wadah yang berisi beras" <<endl;
+cout<< "4. cuci beras dengan air di dalam wadah tersebut" <<endl;
+cout<< "5. buang air cucian beras yang ada di dalam wadah" <<endl;
+cout<< "6. masukan air ke dalam wadah satu cm di atas permukaan beras" <<endl;
+cout<< "7. nyalakan rice cooker" <<endl;
+cout<< "8. masukan air dan beras yang ada di dalam wadah ke rice cooker" <<endl;
+cout<< "9. tekan tombol on pada rice cooker" <<endl;
+cout<< "10. tutup rice cooker" <<endl;
+cout<< "11. tunggu beras sampe menjadi matang" <<endl;
+cout<< "12. nasi siap di hidangkan" <<endl;
+
+
+return 0; 
+
+}
