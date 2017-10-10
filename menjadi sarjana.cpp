6711208ce1c5c2934e61d989da26d2dbfb2cfee3
Dedy Mulyana <32387341+Dedymulyana@users.noreply.github.com>
2017-10-10 22:13:44 +0700

    Add files via upload

---
+++ menjadi sarjana.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+	cout << "1. Daftar Sekolah Dasar" <<endl;
+	cout << "2. masuk kelas I SD"<<endl;
+	cout << "3. masuk kelas II SD"<<endl;
+	cout << "4. masuk kelas III SD"<<endl;
+	cout << "5. masuk kelas IV SD"<<endl;
+	cout << "6. masuk kelas V SD"<<endl;
+	cout << "7. masuk kelas VI SD"<<endl;
+	cout << "8. lulus Sekolah Dasar"<<endl;
+
+
+
+	cout << "===============================" <<endl;
+
+
+
+	cout << "1. Daftar Sekolah Menengah Pertama"<<endl;
+	cout << "2. masuk kelas I SMP"<<endl;
+	cout << "3. masuk kelas II SMP"<<endl;
+	cout << "4. masuk kelas III SMP"<<endl;
+	cout << "5. Lulus Sekolah Menengah Pertama"<<endl;
+
+
+	cout << "===============================" <<endl;
+
+
+
+	cout << "1. Daftar Sekolah Menengah Kejuruan"<<endl;
+	cout << "2. masuk kelas I SMK"<<endl;
+	cout << "3. mausk kelas II SMK"<<endl;
+	cout << "4. masuk kelas III SMK"<<endl;
+	cout << "5. lulus Sekolah Menengah Kejuruan"<<endl;
+
+
+	cout << "===============================" <<endl;
+
+	
+
+	cout << "Daftar Perguruan Tinggi"<<endl;
+	cout << "Masuk semester I"<<endl;
+	cout << "Masuk semester II"<<endl;
+	cout << "Masuk semester III"<<endl;
+	cout << "Masuk semester IV"<<endl;
+	cout << "Masuk semester V"<<endl;
+	cout << "Masuk semeste VI"<<endl;
+	cout << "Masuk semester VII"<<endl;
+	cout << "Masuk semester VIII"<<endl;
+	cout << "Mulai Skripsi"<<endl;
+	cout << "Sidang"<<endl;
+	cout << "Menjadi Seorang Sarjana"<<endl;
+
+	return 0;
+}
+
+
