3ca24284bc51143ea4ad3d0875ac12288ee9eb8a
Dedy Mulyana <32387341+Dedymulyana@users.noreply.github.com>
2017-11-28 21:59:02 +0700

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string nama[10];
+
+	for(int i=0; i<10; i++){
+
+		cin >> nama [i];
+	}
+
+
+	cout << "==========================" <<endl;
+
+	for (int i=0; i<10; i++){
+
+		cout << "tampilkan : " << nama [i] <<endl;
+	}
+
+	return 0;
+}
